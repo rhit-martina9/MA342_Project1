6c742a60ea32040ee0744c4780edaa3727297a16
rhit-davisoc <78520741+rhit-davisoc@users.noreply.github.com>
2023-03-11 17:50:38 -0500

    Dose-point calculation updates

--- DoseCoefficient.m
+++ DoseCoefficient.m
@@ -1,36 +1,42 @@
 function [dose] = DoseCoefficient(p,a,i)
 
-p_angle = 5;
-
 %Given values for 6MV machine, 3mm square pencil
 P0 = 0.873;
 u = 0.0469;
-r = .0016830;
+r = .0016830; %Pencil radius
 gam = 5.2586;
 
 %Adjusted values
-cr = .05; %Circle radius.
-lgc = 1;
+Rc = .05; %Circle radius.
+lgc = 1; %Length of gantry to center
 
 %Calculated values
-s = lgc - cr;
-gx = cos(a);
-gy = sin(a);
+gx = cos(a); %X-coord of gantry
+gy = sin(a);%Y-coord of gantry
 
-lgp = sqrt((x-gx)^2 + (y-gy)^2);
+lgp = sqrt((p(1)-gx)^2 + (p(2)-gy)^2);
 
-d = lgp*cos(p_angle) - s;
-o = lgp*sin(p_angle);
+f = atan(r/lgc); %Angle of center pencil line to pencil edge
+theta = 2*i*f; %Angle from center pencile line to center line
 
-ad = 0.1299 - 0.0306*ln(d);
 ISF = (lgc/lgp)^2;
 
-psi = (o*lgc)/(s+d);
+fs = @(s) lgc^2 + s^2 -2*lgc*s*cos(theta) - Rc^2;
+s = fsolve(fs,0);
 
-if(d > .015)
-    dose = P0*exp(-u*(d-.015))*(1 - exp(-gam*r)) + ((r*d*ad)/(r+1.5));
+a2 = a + theta; %Angle between pencil center line and horizontal line
+o = abs(-(p(1)-gx)*sin(a2) + (p(2)-gy)*cos(a2));
+
+d = -s + sqrt(lgp^2-o^2);
+ad = (0.1299 - 0.0306*log(d));
+
+psi = (o*lgc)/(s+d);
+O = OffAxisFactor(psi);
+
+if(d >= .015)
+    dose = P0*exp(-u*(d-.015))*(1 - exp(-gam*r)) + ((r*d*ad)/(r+1.5))*O*ISF;
 else
-    dose = (0.4*(d/1.5) + 0.6)*(P0*(1-exp(-gam*r))) + ((1.5*r*ad)/(r+1.5));
+    dose = (0.4*(d/1.5) + 0.6)*(P0*(1-exp(-gam*r))) + ((1.5*r*ad)/(r+1.5))*O*ISF;
 end
 
 end
--- OffAxisFactor.m
+++ OffAxisFactor.m
@@ -2,7 +2,31 @@ function [mult] = OffAxisFactor(psi)
 
 M = readmatrix('OffAxisData.txt');
 
+len = length(M);
+index = 2;
 
+if(psi < .044270)
+    mult = 1;
+    return;
+end
+
+if(psi > M(340,1))
+   mult = M(340,2);
+   return;
+end
+
+%Find indexes psi lies between. Lower is index - 1, upper is index.
+while(~(psi < M(index) && psi > M(index-1)) && index < len)
+    index = index + 1;
+end
+
+%Linear interpolation
+x1 = M(index-1,1);
+x2 = M(index,1);
+y1 = M(index-1,2);
+y2 = M(index,2);
+
+mult = ((y2-y1)/(x2-x1))*(psi-x1) + y1;
 
 end
 
