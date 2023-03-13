1b2a020e3820695828cac0a2fd937263e1c599a9
rhit-davisoc <78520741+rhit-davisoc@users.noreply.github.com>
2023-03-13 12:09:38 -0400

    DoseCoefficient function to work with matrices

--- DoseCoefficient.m
+++ DoseCoefficient.m
@@ -1,43 +1,45 @@
-function [dose] = DoseCoefficient(p,a,i)
+function [dose] = DoseCoefficient(x,y,a,i)
 
 %Given values for 6MV machine, 3mm square pencil
 P0 = 0.873;
 u = 0.0469;
-r = .0016830; %Pencil radius
+r = .16830; %Pencil radius
 gam = 5.2586;
 
 %Adjusted values
-Rc = .05; %Circle radius.
-lgc = 1; %Length of gantry to center
+Rc = 5; %Circle radius.
+lgc = 100; %Length of gantry to center
 
 %Calculated values
-gx = cos(a); %X-coord of gantry
-gy = sin(a);%Y-coord of gantry
+gx = lgc*cos(a); %X-coord of gantry
+gy = lgc*sin(a);%Y-coord of gantry
 
-lgp = sqrt((p(1)-gx)^2 + (p(2)-gy)^2);
+lgp = sqrt((x-gx).^2 + (y-gy).^2);
 
 f = atan(r/lgc); %Angle of center pencil line to pencil edge
 theta = 2*i*f; %Angle from center pencile line to center line
 
-ISF = (lgc/lgp)^2;
+ISF = (lgc./lgp).^2;
 
 fs = @(s) lgc^2 + s^2 -2*lgc*s*cos(theta) - Rc^2;
 s = fsolve(fs,0);
 
 a2 = a + theta; %Angle between pencil center line and horizontal line
-o = abs(-(p(1)-gx)*sin(a2) + (p(2)-gy)*cos(a2));
+o = abs(-(x-gx)*sin(a2) + (y-gy)*cos(a2));
 
-d = -s + sqrt(lgp^2-o^2);
+d = -s + sqrt(lgp.^2.-o.^2);
 ad = (0.1299 - 0.0306*log(d));
 
-psi = (o*lgc)/(s+d);
-O = OffAxisFactor(psi);
+psi = (o.*lgc)./(s+d);
+O = arrayfun(@(x)OffAxisFactor(x),psi);
 
-if(d >= .015)
-    dose = P0*exp(-u*(d-.015))*(1 - exp(-gam*r)) + ((r*d*ad)/(r+1.5))*O*ISF;
-else
-    dose = (0.4*(d/1.5) + 0.6)*(P0*(1-exp(-gam*r))) + ((1.5*r*ad)/(r+1.5))*O*ISF;
-end
+dose1 = (P0*exp(-u*(d-1.5)).*(1 - exp(-gam*r)) + ((r*d.*ad)./(r+1.5))).*O.*ISF;
+dose1 = dose1.*(d >= 1.5);
+    
+dose2 = (0.4.*(d./1.5) + 0.6).*(P0*(1-exp(-gam*r)) + ((1.5*r.*ad)./(r+1.5))).*O.*ISF;
+dose2 = dose2.*(d < 1.5);
+
+dose = dose1 + dose2;
 
 end
 
